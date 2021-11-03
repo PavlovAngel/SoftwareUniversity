0527d30a68ca3b157b155dbfe0ad6a5e4c2d7eca
angelpavlov <pavlovangel12@gmail.com>
2021-11-03 22:34:18 +0200

    Add homework

--- Homework/ProgrammingBasics/FirstStepsInCoding/ConcatenateData.java
+++ Homework/ProgrammingBasics/FirstStepsInCoding/ConcatenateData.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class ConcatenateData {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        String FirstName = scanner.nextLine();
-        String LastName = scanner.nextLine();
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
         int age = Integer.parseInt(scanner.nextLine());
-        String Town = scanner.nextLine();
-        System.out.print("You are " + FirstName + " " + LastName +", a " + age + "-years old person from " + Town + ".");
+        String town = scanner.nextLine();
+        System.out.printf("You are %s %s, a %d -years old person from %s.",firstName,lastName,age,town);
 
     }
 }

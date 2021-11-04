712895136228ac135e9daa7dea3653289792739b
angelpavlov <pavlovangel12@gmail.com>
2021-11-04 21:08:05 +0200

    Add homework

---
+++ Homework/ProgrammingBasics/FirstStepsInCoding/ProjectsCreation.java
@@ -0,0 +1,13 @@
+package ProgrammingBasics.FirstStepsInCoding;
+
+import java.util.Scanner;
+
+public class ProjectsCreation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String architect = scanner.nextLine();
+        int projects  = Integer.parseInt(scanner.nextLine());
+        int hours  = projects * 3;
+        System.out.println("The architect " + architect + " will need " + hours + " hours to complete " + projects + " project/s.");
+    }
+}

e3ed5211b0ce2db320cbcf5f605a65f372732d77
angelpavlov <pavlovangel12@gmail.com>
2021-11-05 18:25:40 +0200

    Add homework

---
+++ Homework/ProgrammingBasics/FirstStepsInCodingExercise/DepostiCalculator.java
@@ -0,0 +1,16 @@
+package ProgrammingBasics.FirstStepsInCodingExercise;
+
+import java.util.Scanner;
+
+public class DepostiCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double sum = Double.parseDouble(scanner.nextLine());
+        int mounth = Integer.parseInt(scanner.nextLine());
+        double yearpercent = Double.parseDouble(scanner.nextLine());
+        double yearpercent2 = yearpercent  / 100;
+        System.out.println(sum + mounth * ((sum * yearpercent2) /12));
+
+    }
+
+}

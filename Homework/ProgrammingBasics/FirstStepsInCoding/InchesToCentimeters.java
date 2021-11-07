73b350030fdf9e20a2d6226bcdb9c362b53162e6
angelpavlov <pavlovangel12@gmail.com>
2021-11-07 13:33:07 +0200

    Add homework

--- Homework/ProgrammingBasics/FirstStepsInCoding/InchesToCentimeters.java
+++ Homework/ProgrammingBasics/FirstStepsInCoding/InchesToCentimeters.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class InchesToCentimeters {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double Centimeters = Double.parseDouble(scanner.nextLine());
+        double centimeters = Double.parseDouble(scanner.nextLine());
         double b = 2.54 ;
-        double inches = Centimeters * b ;
+        double inches = centimeters * b ;
         System.out.println(inches);
 
     }

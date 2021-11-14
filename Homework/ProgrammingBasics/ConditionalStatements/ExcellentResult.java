1ed87fa6ed48c42ea7c4a1c98ea2f9546969afad
angelpavlov <pavlovangel12@gmail.com>
2021-11-14 14:48:28 +0200

    Add homework

---
+++ Homework/ProgrammingBasics/ConditionalStatements/ExcellentResult.java
@@ -0,0 +1,13 @@
+package ProgrammingBasics.ConditionalStatements;
+
+import java.util.Scanner;
+
+public class ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int grade = Integer.parseInt(scanner.nextLine());
+        if (grade > 4){
+            System.out.println("Excellent!");
+        }
+    }
+}
